4a3f1f52c49e3f72e762f5b15d1155a044ef22b3
abhiram haridas <abhiramharidas@gmail.com>
2018-02-22 22:48:37 +0530

    Pulling request data correctly

--- java/in/ac/nitc/projectallocator/FacultyMain.java
+++ java/in/ac/nitc/projectallocator/FacultyMain.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -21,22 +22,70 @@ import java.util.Map;
 
 
 public class FacultyMain extends AppCompatActivity {
-    private static final String TAG = "Test";
+    private static final String TAG = "FacultyMain";
+    private DatabaseReference mDatabase;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty_main);
         Log.d(TAG, "Inside Faculty ");
     }
 
 
-    public void signOut(View view) {
+    public void signOut(View view)
+    {
         Log.d(TAG, "Signing Out ");
         FirebaseAuth.getInstance().signOut();
         Intent myIntent = new Intent(this, SignIn.class);
         this.startActivity(myIntent);
     }
+
+    public void projectRequestHandler(View view)
+    {
+        getRequestData();
+    }
+
+    public void getRequestData()
+    {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        DatabaseReference requestQueueRef = mDatabase.child("RequestQueue");
+        final ArrayList<request> requestList = new ArrayList<>();
+
+
+        ValueEventListener requestListener = new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                for(DataSnapshot requestsnapshot : dataSnapshot.getChildren())
+                {
+                    requestList.add(requestsnapshot.getValue(request.class));
+                }
+
+                Log.d(TAG, "requestList.get(0).faculties.get(0) : " + requestList.get(1).faculties.get(2));
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "LoadRequest:onCancelled", databaseError.toException());
+                // ...
+            }
+
+        };
+
+        requestQueueRef.addValueEventListener(requestListener);
+
+
+        }
+
+
 
 
 
--- java/in/ac/nitc/projectallocator/request.java
+++ java/in/ac/nitc/projectallocator/request.java
@@ -3,6 +3,7 @@ package in.ac.nitc.projectallocator;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,5 +13,34 @@ import java.util.Map;
 
 public class request
 {
-    JSONObject requestid;
+    ArrayList< String> areas;
+    ArrayList<String> faculties;
+    String group;
+
+    public ArrayList<String> getAreas()
+    {
+        return this.areas;
+    }
+
+    public ArrayList<String> getFaculties()
+    {
+        return this.faculties;
+    }
+
+    public String getGroup()
+    {
+        return this.group;
+    }
+
+    public request()
+    {
+        //empty constructor
+    }
+
+    public request(ArrayList< String> areas, ArrayList<String> faculties, String group)
+    {
+        this.areas = areas;
+        this.faculties = faculties;
+        this.group = group;
+    }
 }
